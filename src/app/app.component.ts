d47bc1422adab3ba906dcb05bd872f74a545ec2d
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-03 20:38:37 +0200

    Improved SideNav logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  opened = false;
+
   title = 'footyStatsAdminPanel';
+
+  toggleSideMenu() {
+    this.opened = !this.opened;
+  }
 }
