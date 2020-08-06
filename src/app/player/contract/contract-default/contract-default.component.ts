c2fae90458d3142a288c0682c8680bf34a567ab3
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-06 14:52:12 +0200

    Connected the ContractItem with a player

--- src/app/player/contract/contract-default/contract-default.component.ts
+++ src/app/player/contract/contract-default/contract-default.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Contract } from 'src/app/shared/models/contract.model';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ContractNewComponent } from '../contract-new/contract-new.component';
+import { Player } from 'src/app/shared/models/player.model';
 
 @Component({
   selector: 'app-contract-default',
@@ -9,6 +10,8 @@ import { ContractNewComponent } from '../contract-new/contract-new.component';
   styleUrls: ['./contract-default.component.css'],
 })
 export class ContractDefaultComponent implements OnInit {
+  @Input() player: Player;
+
   contracts: Contract[] = [
     { endDate: new Date(), startDate: new Date(), weeklySalary: 1000 },
     { endDate: new Date(), startDate: new Date(), weeklySalary: 12000 },
