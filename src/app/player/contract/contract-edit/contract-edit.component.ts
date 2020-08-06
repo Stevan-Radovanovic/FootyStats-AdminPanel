1d0ba6d137157bb9e4d35cabb12960449d56b2a0
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-06 17:35:14 +0200

    Edit BottomSheet designed

--- src/app/player/contract/contract-edit/contract-edit.component.ts
+++ src/app/player/contract/contract-edit/contract-edit.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {
+  MAT_BOTTOM_SHEET_DATA,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contract-edit',
   templateUrl: './contract-edit.component.html',
-  styleUrls: ['./contract-edit.component.css']
+  styleUrls: ['./contract-edit.component.css'],
 })
 export class ContractEditComponent implements OnInit {
+  editContractForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public bottomSheetRef: MatBottomSheetRef<ContractEditComponent>,
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: any
+  ) {}
 
-  ngOnInit(): void {
+  initForm() {
+    this.editContractForm = new FormGroup({
+      weeklySalary: new FormControl(this.data.contract.weeklySalary, [
+        Validators.required,
+      ]),
+      startDate: new FormControl(this.data.contract.startDate, [
+        Validators.required,
+      ]),
+      endDate: new FormControl(this.data.contract.endDate, [
+        Validators.required,
+      ]),
+    });
   }
 
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  close() {
+    this.bottomSheetRef.dismiss();
+  }
+
+  openLink(event: MouseEvent): void {
+    this.bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
 }
