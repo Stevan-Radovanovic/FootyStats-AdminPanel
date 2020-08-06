80fb7cca2d3d59689aa1deec540489407c559db1
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-06 17:39:28 +0200

    New BottomSheet designed

--- src/app/player/contract/contract-new/contract-new.component.ts
+++ src/app/player/contract/contract-new/contract-new.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {
+  MatBottomSheetRef,
+  MAT_BOTTOM_SHEET_DATA,
+} from '@angular/material/bottom-sheet';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contract-new',
   templateUrl: './contract-new.component.html',
-  styleUrls: ['./contract-new.component.css']
+  styleUrls: ['./contract-new.component.css'],
 })
 export class ContractNewComponent implements OnInit {
+  newContractForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public bottomSheetRef: MatBottomSheetRef<ContractNewComponent>,
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: any
+  ) {}
 
-  ngOnInit(): void {
+  initForm() {
+    this.newContractForm = new FormGroup({
+      weeklySalary: new FormControl('', [Validators.required]),
+      startDate: new FormControl('', [Validators.required]),
+      endDate: new FormControl('', [Validators.required]),
+    });
   }
 
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  close() {
+    this.bottomSheetRef.dismiss();
+  }
+
+  openLink(event: MouseEvent): void {
+    this.bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
 }
