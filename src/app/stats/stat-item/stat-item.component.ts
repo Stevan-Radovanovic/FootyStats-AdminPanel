9b3d28183dcbb71e54ff3dcf0df9368f1b7a39a5
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-16 16:14:29 +0200

    Minor styling changes

--- src/app/stats/stat-item/stat-item.component.ts
+++ src/app/stats/stat-item/stat-item.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Statistic } from 'src/app/shared/models/statistic.model';
 
 @Component({
   selector: 'app-stat-item',
   templateUrl: './stat-item.component.html',
-  styleUrls: ['./stat-item.component.css']
+  styleUrls: ['./stat-item.component.css'],
 })
 export class StatItemComponent implements OnInit {
+  @Input() stat: Statistic;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
