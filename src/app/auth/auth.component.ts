aa915f95f975759593a965818c37938bc22d16ea
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-18 22:17:57 +0200

    Connected log in page to home page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { SessionService } from '../shared/services/session.service';
 import { AuthService } from '../shared/services/auth.service';
+import { Admin } from '../shared/models/admin.model';
 
 @Component({
   selector: 'app-auth',
@@ -18,6 +19,14 @@ export class AuthComponent implements OnInit {
       userName: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required]),
     });
+  }
+
+  logIn() {
+    const admin: Admin = {
+      userName: this.authForm.controls.userName.value,
+      password: this.authForm.controls.password.value,
+    };
+    this.authServ.logIn(admin);
   }
 
   ngOnInit(): void {
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Admin } from '../models/admin.model';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   logIn(admin: Admin) {
     this.http
-      .post<{ token: string }>(
-        'http://localhost:3000/admins/login/' + admin.userName,
-        {
-          password: admin.password,
-        }
-      )
+      .post<{ token: string }>('http://localhost:3000/admins/login', {
+        name: admin.userName,
+        password: admin.password,
+      })
       .subscribe((result) => {
         console.log(result);
+        if (result.token) {
+          this.router.navigateByUrl('/home');
+        }
       });
   }
 }
